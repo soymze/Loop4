5d521611f3032c08d0243fff11f7e7f974ac1245
soymze <soylmze@gmail.com>
2024-01-23 09:33:54 +0300

    asd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int sayi, toplam =0;
+
+        Scanner sc = new Scanner(System.in);
+
+        do{
+            System.out.println("Sayı giriniz  :");
+            sayi = sc.nextInt();
+
+            if(sayi % 4 == 0){
+                toplam += sayi;
+                System.out.println(""+sayi);
+            }
+        }while(sayi % 2 != 1);
+        System.out.println("Toplam  :"+toplam);
     }
 }
